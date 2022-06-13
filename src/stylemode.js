a64ac2664c443351e344c16ac059ee6eb4f0eddb
Frans Billquist <frans.billquist@gmail.com>
2022-06-13 14:52:39 +0200

    rework2

--- src/stylemode.js
+++ src/stylemode.js
@@ -9,12 +9,6 @@ function OnSwitch() {
   const css = document.getElementById("cssSwitcher");
   const darkModeButton = document.getElementById("switchbutton");
 
-  var element = document.getElementById("switchbutton");
-
-  if (element.classList.contains("Light Mode")) {
-    element.addEventListener("click", swapper, false);
-  }
-
   if (darkModeButton.innerHTML == "Dark Mode") {
     css.setAttribute("href", "src/lightstyle.css");
     darkModeButton.innerHTML = "Light Mode";
