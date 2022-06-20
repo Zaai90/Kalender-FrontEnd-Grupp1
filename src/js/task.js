a134bbe0467a9826ea420e239bb5a2a8a1ca9e6a
Daniel T <90195897+DTSD21@users.noreply.github.com>
2022-06-20 15:22:04 +0200

    change so that tasks is updated instead of an array built on ls-data

--- src/js/task.js
+++ src/js/task.js
@@ -15,7 +15,7 @@ function createTask(event) {
   const task = Object.fromEntries(formData.entries());
   task.id = getTaskId();
   addTask(task);
-  event.target.reset(); 
+  event.target.reset();
   updateTaskFormDate(task.taskDate);
 }
 
@@ -100,7 +100,7 @@ function renderTask(task) {
   taskList.appendChild(document.createElement("hr"));
 }
 
-function updateTaskFormDate(date){
+function updateTaskFormDate(date) {
   document.querySelector(`[name="taskDate"]`).value = date;
 }
 
@@ -109,25 +109,22 @@ function editTask(event) {
   const formData = new FormData(event.target);
   const task = Object.fromEntries(formData.entries());
   const taskId = event.target.id.replace("taskEditForm", "");
-  const taskArray = getFromLocalStorage("taskArray");
-  const taskIndex = taskArray.indexOf(
-    taskArray.find((task) => task.id == taskId)
-  );
+  const taskIndex = tasks.indexOf(tasks.find((task) => task.id == taskId));
 
   if (task.taskName) {
-    taskArray[taskIndex].taskName = task.taskName;
+    tasks[taskIndex].taskName = task.taskName;
   }
 
   if (task.taskDescription) {
-    taskArray[taskIndex].taskDescription = task.taskDescription;
+    tasks[taskIndex].taskDescription = task.taskDescription;
   }
 
   if (task.taskDate) {
-    taskArray[taskIndex].taskDate = task.taskDate;
+    tasks[taskIndex].taskDate = task.taskDate;
   }
 
-  saveToLocalStorage("taskArray", taskArray);
-  renderAllTasks();
+  saveToLocalStorage("taskArray", tasks);
+  update();
 }
 
 function addTaskHtml() {
